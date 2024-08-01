279e3589479d7d28c8931db93a5277777113d087
Ambikesh Kumar <152788402+AbhiAmbikesh@users.noreply.github.com>
2024-08-01 19:42:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const slider = document.querySelector('.slider');
-
-function activate() {
-  const items = document.querySelectorAll('.item');
-  slider.append(items[0]);
-}
-
-document.querySelector('.slider').addEventListener('click', activate);
